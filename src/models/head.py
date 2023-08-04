8e045cc6496f607d089f2b469deb9e914e3bb3af
EmperorAkashi <linchen4869@ucla.edu>
2023-08-04 17:59:47 -0400

    add svd head

--- src/models/head.py
+++ src/models/head.py
@@ -2,3 +2,60 @@ import numpy as np
 import torch
 import torch.nn as nn
 
+class SVDHead(nn.Module):
+    def __init__(self, args) -> None:
+        super().__init__()
+        self.emb_dims = args.emb_dims
+        self.reflect = nn.Parameter(torch.eye(3), requires_grad=False)
+
+    def forward(self, *input) -> torch.Tensor:
+        """the input should be tuple of embedded features and 
+        raw clouds
+        """
+        src_embedding = input[0]
+        tgt_embedding = input[1]
+        src = input[2]
+        tgt = input[3]
+        batch_size = src.size(0)
+
+        d_k = src_embedding.size(1)
+        # cross score/pointer map of phi_x and phi_y
+        scores = torch.matmul(src_embedding.transpose(2, 1).contiguous(), tgt_embedding) / math.sqrt(d_k)
+        scores = torch.softmax(scores, dim=2)
+
+        # apply pointer map to "correct the raw target cloud"
+        src_corr = torch.matmul(tgt, scores.transpose(2,1).contiguous())
+
+        # centralize two clouds for the relative rotation
+        src_centered = src - src.mean(dim=2, keepdim=True)
+        src_corr_centered = src_corr - src_corr.mean(dim=2, keepdim=True)
+
+        # start regular svd calculation
+        H = torch.matmul(src_centered, src_corr_centered.transpose(2, 1).contiguous())
+
+        U, S, V = [], [], []
+        R = []
+
+        for i in range(src.size(0)):
+            u, s, v = torch.svd(H[i])
+            r = torch.matmul(v, u.transpose(1, 0).contiguous())
+            r_det = torch.det(r)
+            if r_det < 0:
+                u, s, v = torch.svd(H[i])
+                v = torch.matmul(v, self.reflect)
+                r = torch.matmul(v, u.transpose(1, 0).contiguous())
+                # r = r * self.reflect
+            R.append(r)
+
+            U.append(u)
+            S.append(s)
+            V.append(v)
+
+        U = torch.stack(U, dim=0)
+        V = torch.stack(V, dim=0)
+        S = torch.stack(S, dim=0)
+        R = torch.stack(R, dim=0)
+
+        # retrieve translation from rotated points
+        t = torch.matmul(-R, src.mean(dim=2, keepdim=True)) + src_corr.mean(dim=2, keepdim=True)
+        return R, t.view(batch_size, 3)
