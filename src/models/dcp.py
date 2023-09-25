96eb4f3bd8293324d06426546398bdccb729ef2d
EmperorAkashi <linchen4869@ucla.edu>
2023-09-25 18:00:21 -0400

    update dcp module

--- src/models/dcp.py
+++ src/models/dcp.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import models.config as cf
 from models.dgcnn import DGCNN
 import models.head as H
+from models.transformer import Transformer
 
 class DCP(nn.Module):
     # TO DO: both head and backbone should be wrap up as a factory pattern
@@ -16,17 +17,25 @@ class DCP(nn.Module):
         else:
             raise Exception("Not a valid backbone")
 
+        if cf.pointer == "transformer":
+            self.pointer = Transformer(cf.transform)
+
         if cf.head == "svd":
             self.head = H.SVDHead(self.emb_dims)
 
     def forward(self, input_data:torch.Tensor) -> torch.Tensor:
+        """the transformer mechanism generate a relative probability 
+        map for the correspondence x and y;
+        by multiplying this map, 
+        it shift the raw cloud y to a matched averaged point y_hat
+        """
         src = input_data[0]
         tgt = input_data[1]
 
         src_embedding = self.emb_nn(src)
         tgt_embedding = self.emb_nn(tgt)
 
-        src_embedding_p, tgt_embedding_p = self.emb_nn(src_embedding, tgt_embedding)
+        src_embedding_p, tgt_embedding_p = self.pointer(src_embedding, tgt_embedding)
 
         src_embedding = src_embedding + src_embedding_p
         tgt_embedding = tgt_embedding + tgt_embedding_p
